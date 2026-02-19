0a7cd8038c5582b23d9585456b722e11ce32173d
Alexander T Korsor <alexandertkorsor1@gmail.com>
2026-02-19 12:50:01 +0530

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import './index.css';
 import './LogoStyles.css';
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <HashRouter>
+  <BrowserRouter basename="/e-portoralex">
     <App />
-  </HashRouter>
+  </BrowserRouter>
 );
